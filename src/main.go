90a3ecd156255447836ab96e06952d77d5f5b7a1
junior <junior@beetech.global>
2017-11-16 14:56:49 -0200

    feat(static routes): add status routes to return html pages

--- src/main.go
+++ src/main.go
@@ -8,8 +8,13 @@ import (
 
 func main() {
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/**/*.html")
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello go %v", "Gin")
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	r.GET("/login", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "login.html", nil)
 	})
 
 	r.Run(":3000")
